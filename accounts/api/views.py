91a488db405dedb9730f86471882e9babcd2ba69
Zhekang Peng <zhekangpeng@Zhekangs-MacBook-Pro.local>
2023-09-10 23:56:06 -0400

    unit tests for user login/logout/signup

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -56,7 +56,7 @@ class AccountViewSet(viewsets.ViewSet):
         return Response({
             "success": True,
             "user": UserSerializer(instance=user).data
-        })
+        }, status=status.HTTP_200_OK)
 
     @action(methods=['POST'], detail=False)
     def logout(self, request):
